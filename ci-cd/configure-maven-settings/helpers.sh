d0c8898ada2acf299cb709a7567f2d62b173132e
Peder Schmedling <24737456+Laffs2k5@users.noreply.github.com>
2023-06-02 10:08:47 +0200

    feat: new action `configure-maven-settings`

---
+++ ci-cd/configure-maven-settings/helpers.sh
@@ -0,0 +1,21 @@
+#!/bin/env bash
+
+# Helper consts
+_action_name='configure-maven-settings'
+
+# Helper functions
+function _log { echo "${1}${_action_name}: ${2}"; }
+function log-info { _log "" "${*}"; }
+function log-debug { _log "DEBUG: " "${*}"; }
+function log-warn { _log "WARN: " "${*}"; }
+function log-error { _log "ERROR: " "${*}"; }
+function start-group { echo "::group::${_action_name}: ${*}"; }
+function end-group { echo "::endgroup::"; }
+function log-multiline {
+  start-group "${1}"
+  echo "${2}"
+  end-group
+}
+function mask-value { echo "::add-mask::${*}"; }
+
+log-info "'$(basename ${BASH_SOURCE[0]})' loaded."
