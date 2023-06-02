b388082314169fa62a732d84dd17547ffc03d4cb
Peder Schmedling <24737456+Laffs2k5@users.noreply.github.com>
2023-06-02 10:08:01 +0200

    chore: create-app-vars-matrix: test: support clean and handle errors

--- ci-cd/create-app-vars-matrix/test_action_source.sh
+++ ci-cd/create-app-vars-matrix/test_action_source.sh
@@ -10,7 +10,19 @@ this_script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 action_def_file="${this_script_dir}/action.yml"
 
 # load test runner code
-source "${this_script_dir}/test_action_source_helpers.sh"
+source "${this_script_dir}/test_action_source_helpers.sh" $*
+
+if [ "${1-}" == "clean" ]; then
+  del_files=$(find "${this_script_dir}" -name '_*' -print | sort)
+  if [ ! -z "${del_files}" ]; then
+    echo "deleting these:"
+    echo "${del_files}" | xargs realpath --relative-to=$(pwd)
+    echo "${del_files}" | xargs rm
+  else
+    echo "nothing to delete"
+  fi
+  exit 0
+fi
 
 # run tests
 should_pass_test 'test_input_minimal' "${action_def_file}" "${this_script_dir}"
