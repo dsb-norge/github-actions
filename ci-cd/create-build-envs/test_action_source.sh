73918a079008147fee4ee97d62a6ab4a34cdf3fa
Peder Schmedling <24737456+Laffs2k5@users.noreply.github.com>
2023-06-02 10:37:40 +0200

    feat: maven settings and deploy repos: - new parameter `maven-extra-envs-from-github-yml` makes it possible to export environment variables available to maven with values from the contexts `vars`, `secrets` and `github` in github - major (non-breaking) changes related to maven settings.xml:   - no long use `actions/setup-java` to manage maven settings.xml   - write settings.xml with our own `configure-maven-settings`     - contents of settings.xml is now configurable through the parameter `maven-user-settings-repositories-yml`     - it's possible to reference secrets/vars from github in `maven-user-settings-repositories-yml` with the combination of maven's interpolation syntax `${env.MY_ENV}` and new parameter `maven-extra-envs-from-github-yml`. See default values in create-build-envs for example. - major (non-breaking) changes related to deploy of artifacts with maven:   - deploy og snapshots/releases is no longer hardcoded to one single maven repo   - new parameter `maven-build-project-deploy-to-repositories-yml` makes repositories individually configurable for snapshots/releases. Note that credentials for these repos must be configured using the parameter `maven-user-settings-repositories-yml` - other changes:   - parameters no longer in use:     - `maven-repo-pom-id`     - `maven-repo-username`     - `maven-repo-token`     - `maven-build-project-github-packages-enabled`   - 'create-build-envs':     - bash test script added to test action locally   - 'build-maven-project':     - make sure to unset extra maven environment variables after use so they are not available/exposed to subsequent actions   - 'build-maven-project', 'create-build-envs' and 'run-maven-command':     - use `env:` action input for multiline strings in     - log less when loading helper script     - bash helper script added

---
+++ ci-cd/create-build-envs/test_action_source.sh
@@ -0,0 +1,31 @@
+#!/bin/env bash
+#
+# A way of actually running the bash code from this github action locally during development
+#
+set -uo pipefail
+
+this_script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+# the github actions def to test
+action_def_file="${this_script_dir}/action.yml"
+
+# load test runner code
+source "${this_script_dir}/test_action_source_helpers.sh"
+
+if [ "${1-}" == "clean" ]; then
+  del_files=$(find "${this_script_dir}" -name '_*' -print | sort)
+  if [ ! -z "${del_files}" ]; then
+    echo "deleting these:"
+    echo "${del_files}" | xargs realpath --relative-to=$(pwd)
+    echo "${del_files}" | xargs rm
+  else
+    echo "nothing to delete"
+  fi
+  exit 0
+fi
+
+# run tests
+should_pass_test 'test_input_minimal' "${action_def_file}" "${this_script_dir}"
+should_pass_test 'test_input_happy_day' "${action_def_file}" "${this_script_dir}"
+should_pass_test 'test_input_helm_values_from_input' "${action_def_file}" "${this_script_dir}"
+should_fail_test 'test_input_fail_yml_format' "${action_def_file}" "${this_script_dir}"
