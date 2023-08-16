8841764d8650dbe9db1102fd794e25915d3bf4b3
Peder Schmedling <24737456+Laffs2k5@users.noreply.github.com>
2023-08-16 22:14:19 +0200

    feat: prune-maven-artifacts-in-repo: support `dry-run: true`

--- ci-cd/prune-maven-artifacts-in-repo/helpers_additional.sh
+++ ci-cd/prune-maven-artifacts-in-repo/helpers_additional.sh
@@ -3,4 +3,7 @@
 # working with JSON array input
 function get-first-val { echo "${JSON_ARRAY}" | jq -r --arg name "${1}" '. | map(.[$name] | select( . != null ))  | first // empty'; }
 
+# support non-destructive mode
+function is-dry-run { if [ ! 'false' == "${DRY_RUN_INPUT}" ]; then true; else false; fi; }
+
 log-info "'$(basename ${BASH_SOURCE[0]})' loaded."
